d61872bec779a2154f2d25a415ac23c324861b9c
Peyton Lee <gabybiscuits@gmail.com>
2022-10-01 17:39:53 -0700

    refactor: Reorganized files into frontend and backend subfolders

--- ssn-frontend/src/App.js
+++ ssn-frontend/src/App.js
@@ -22,4 +22,15 @@ function App() {
   );
 }
 
+import { addUserAgent } from 'nxapi';
+
+addUserAgent('test-shop-notifications/1.0.0 (+https://github.com/ShrimpCryptid)');
+
+// This could also be read from a package.json file
+import { fileURLToPath } from 'node:url';
+import { resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+const pkg = JSON.parse(await readFile(resolve(fileURLToPath(import.meta.url), '..', 'package.json'), 'utf-8'));
+addUserAgent(pkg.name + '/' + pkg.version + ' (+' + pkg.repository.url + ')');
+
 export default App;

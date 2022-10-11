93ee9376b8f5abd805c7ec8f7c227b692776c735
Peyton Lee <gabybiscuits@gmail.com>
2022-10-10 20:03:50 -0700

    feat: Added types, brands, and gear abilities to filter preview page.

--- pages/filter.tsx
+++ pages/filter.tsx
@@ -1,31 +1,62 @@
-import Head from 'next/head';
-import { useState, useEffect, SetStateAction } from 'react';
-import Selector from '../components/selector';
+/**
+ * Page for creating and editing filters.
+ */
 
-export default function Home() {
+import Head from "next/head";
+import { useState, useEffect, SetStateAction } from "react";
+import {FunctionComponent} from 'react';
+import Selector from "../components/selector";
+import { GEAR_ABILITIES, GEAR_BRANDS, GEAR_TYPES } from "../constants";
+import { Filter } from "../lib/database_utils";
 
-  const [selected, setSelected] = useState(new Map([
-    ['Any', false],
-    ['Ink Saver (Main)', true],
-    ['Ink Saver (Sub)', false],
-    ['Run Speed Up', false],
-    ['Last-Ditch Effort', true],
-    ['Ink Resist Up', true]
-  ]));
 
-  return (
-    <div id="app">
-      <Head>
-        Splatnet Shop Alerts
-      </Head>
-      <p>Placeholder text</p>
-      <Selector
-            items={Array.from(selected.keys())}
-            selected={selected}
-            wildcard={true}
-            onChanged={(newSelected: Map<string, boolean>) => {setSelected(newSelected)}}
-      />
-      <p>Any is {selected.get("Any") ? "true!" : "false!"}</p>
-    </div>
-)
+function listToBooleanMap(arr: string[], initValue=false): Map<string, boolean> {
+    let boolArr: [string, boolean][] = arr.map(value => {return [value, initValue]});
+    return new Map(boolArr);
+}
+
+type FilterProps = {
+    filter?: Filter
+}
+
+export default function FilterPage () {
+
+    // TODO: Handle editing filters
+    const [selectedAbilities, setSelectedAbilities] = useState(listToBooleanMap(GEAR_ABILITIES));
+    const [selectedBrands, setSelectedBrands] = useState(listToBooleanMap(GEAR_BRANDS));
+    const [selectedTypes, setSelectedTypes] = useState(listToBooleanMap(GEAR_TYPES));
+
+	return (
+		<div id="app">
+			<Head>Splatnet Shop Alerts</Head>
+			<p>Placeholder text</p>
+            <h1>Gear Types</h1>
+			<Selector
+				items={Array.from(selectedTypes.keys())}
+				selected={selectedTypes}
+				wildcard={true}
+				onChanged={(newSelected: Map<string, boolean>) => {
+					setSelectedTypes(newSelected);
+				}}
+			/>
+            <h1>Gear Brands</h1>
+			<Selector
+				items={Array.from(selectedBrands.keys())}
+				selected={selectedBrands}
+				wildcard={true}
+				onChanged={(newSelected: Map<string, boolean>) => {
+					setSelectedBrands(newSelected);
+				}}
+			/>
+            <h1>Gear Abilities</h1>
+			<Selector
+				items={Array.from(selectedAbilities.keys())}
+				selected={selectedAbilities}
+				wildcard={true}
+				onChanged={(newSelected: Map<string, boolean>) => {
+					setSelectedAbilities(newSelected);
+				}}
+			/>
+		</div>
+	);
 }

78753cf0b38058d41b72466858c69ffb66d529fa
Peyton Lee <gabybiscuits@gmail.com>
2022-11-14 23:10:43 -0800

    feat: Made loading spinner asset.

--- public/icons/utils/index.tsx
+++ public/icons/utils/index.tsx
@@ -1,5 +1,7 @@
 import none from './none.svg';
 import unknown from './unknown.png';
+import loading from './loading_spinner.svg';
 
 export const noneIcon = none;
 export const unknownIcon = unknown;
+export const loadingIcon = loading;

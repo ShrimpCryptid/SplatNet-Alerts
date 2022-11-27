22f37a0bfa4b51179840acd612fe4f05bca386eb
Peyton Lee <gabybiscuits@gmail.com>
2022-11-27 15:34:57 -0800

    feat: Added switch UI component

---
+++ components/switch.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { FunctionComponent } from "react";
+import styles from './switch.module.css';
+
+type Props = {
+  state: boolean,
+  disabled?: boolean,
+  loading?: boolean,
+  onToggled: (newState: boolean) => void
+}
+
+const Switch: FunctionComponent<Props> = (props) => {
+
+
+  let stateClass = "";
+  stateClass += props.state ? " " + styles.stateTrue : "";
+  stateClass += props.loading ? " " + styles.loading : "";
+  stateClass += props.disabled ? " " + styles.disabled : "";
+
+  let containerClass = styles.sliderContainer + stateClass;
+  let sliderClass = styles.slider + stateClass;
+  return (
+    <button
+      className={containerClass}
+      onClick={() => {
+        if (!props.loading && !props.disabled) {
+          // Don't toggle for loading or disabled states
+          props.onToggled(!props.state);
+        }
+      }}>
+      <div className={sliderClass}
+      >
+        <div style={{width: "100%", height: "100%", position: "relative"}}>
+          <div className={styles.loadingIcon} />
+          <span className={"material-symbols-rounded " + styles.disabledIcon}>error</span>
+        </div>
+
+      </div>
+    </button>
+  );
+}
+
+export default Switch;

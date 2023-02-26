44992e751ce677b988ab488cb75aee5c7b1870c7
Peyton Lee <gabybiscuits@gmail.com>
2023-02-25 23:37:11 -0800

    feat: Added new unknown icon for Z+F brand

--- public/icons/brands/index.tsx
+++ public/icons/brands/index.tsx
@@ -1,26 +1,28 @@
 // Icons are statically imported to allow Next.js to analyze them at build time.
 // See '/lib/helpers.ts' for functions that will generate this code for you.
 
-import amiibo from './amiibo.png'
-import annaki from './annaki.png'
-import barazushi from './barazushi.png'
-import cuttlegear from './cuttlegear.png'
-import emberz from './emberz.png'
-import enperry from './enperry.png'
-import firefin from './firefin.png'
-import forge from './forge.png'
-import grizzco from './grizzco.png'
-import inkline from './inkline.png'
-import krakOn from './krak-on.png'
-import rockenberg from './rockenberg.png'
-import skalop from './skalop.png'
-import splashMob from './splash_mob.png'
-import squidForce from './squidforce.png'
-import takoroka from './takoroka.png'
-import tentatek from './tentatek.png'
-import toniKensa from './toni_kensa.png'
-import zekko from './zekko.png'
-import zink from './zink.png'
+import unknown from './unknown.svg';
+
+import amiibo from './amiibo.png';
+import annaki from './annaki.png';
+import barazushi from './barazushi.png';
+import cuttlegear from './cuttlegear.png';
+import emberz from './emberz.png';
+import enperry from './enperry.png';
+import firefin from './firefin.png';
+import forge from './forge.png';
+import grizzco from './grizzco.png';
+import inkline from './inkline.png';
+import krakOn from './krak-on.png';
+import rockenberg from './rockenberg.png';
+import skalop from './skalop.png';
+import splashMob from './splash_mob.png';
+import squidForce from './squidforce.png';
+import takoroka from './takoroka.png';
+import tentatek from './tentatek.png';
+import toniKensa from './toni_kensa.png';
+import zekko from './zekko.png';
+import zink from './zink.png';
 
 export const brandIcons = new Map(Object.entries({
 	'amiibo': amiibo,
@@ -43,4 +45,5 @@ export const brandIcons = new Map(Object.entries({
 	'Toni Kensa': toniKensa,
 	'Zekko': zekko,
 	'Zink': zink,
+  'Z+F': unknown,
 }));
